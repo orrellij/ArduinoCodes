5f30154e46d7f012ea45bc776f1ff9ece3151e26
orrellij <orrellij@appstate.edu>
2023-11-08 14:41:13 -0500

    First Commit (Only one side of sensor coded.) 	new file:   PressureSensorWill_Ian_DrLee.ino

---
+++ PressureSensorWill_Ian_DrLee.ino
@@ -0,0 +1,37 @@
+ //#define debug
+
+const float vS = 5; // in volts
+int sensorPin = A0;
+float pressureValue = 0;
+const float ADC_V = .0048876;
+const float sensitivity = 90; // mv/kPa
+const float cmH20 = 10.197162129779; // cmH20/kPa
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int pinVal = analogRead(sensorPin);
+  #ifdef debug
+  Serial.println(pinVal);
+  #endif //debug
+
+  float pVal1 = pinVal *ADC_V;
+  #ifdef debug
+  Serial.println(pVal1);
+  #endif //debug 
+
+  pressureValue = ((pVal1 * 1000) / sensitivity) * cmH20;
+  Serial.println(pressureValue);
+ 
+  Serial.print("\n\n");
+
+  delay(1000);
+
+}
